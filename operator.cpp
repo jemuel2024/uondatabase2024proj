c53803d79886c8646c63bf03197238493552cee4
jemuel2024 <odayoalfrred@gmail.com>
2024-04-10 10:42:18 +0300

    c1
    the base control function 1

---
+++ operator.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+void createlist();
+int main(){
+ int option;
+    cout<<"select the operation you want to perform"<<endl;
+cout<<"1.create a list"<<'\n'<<"2.Add members"<<'\n'<<"3.Remove a member "<<'\n'<<"4.Print list"<<endl;
+cin>>option;
+switch(option){
+    case 1:createlist();
+           break;    
+}
+}
+void createlist(){
+    
+}
